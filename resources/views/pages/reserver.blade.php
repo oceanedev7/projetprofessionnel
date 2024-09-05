1e4fe75bca367bfd840fead44fdc5290bac3bb7e
oceanedev7 <oceanepy7@gmail.com>
2024-09-05 11:55:31 -0400

    css du dropdown (profil user)

--- resources/views/pages/reserver.blade.php
+++ resources/views/pages/reserver.blade.php
@@ -20,8 +20,8 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6 absolute top-9 right-24">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->nom }}</div>
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-bold rounded-md bg-white bg-opacity-90 text-custom-vert focus:outline-none transition ease-in-out duration-150">
+                            <div> {{ Auth::user()->prenom }} {{ Auth::user()->nom }} </div>
         
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
