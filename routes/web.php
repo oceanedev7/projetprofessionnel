456a70ea5762c9f02da2fd6d0b7ab89e9c059840
oceanedev7 <oceanepy7@gmail.com>
2024-11-05 11:50:20 -0400

    création d'un groupe de routes réservées à l'admin

--- routes/web.php
+++ routes/web.php
@@ -50,10 +50,6 @@ Route::get('/faq', function () {
     return view('pages.footer.faq');
 })->name('faq');
 
-// Route::get('/contact&acces', function () {
-//     return view('pages.contact');
-// })->name('contact')->middleware(\App\Http\Middleware\Localisation::class);
-
 Route::get('/reserver', function () {
     return view('pages.reserver');
 })->name('reserver');
@@ -62,7 +58,6 @@ Route::get('/noscabanes', function () {
     return view('pages.cabanes.noscabanes');
 })->name('noscabanes')->middleware(\App\Http\Middleware\Localisation::class);
 
-
 Route::get('/cabaneniddouillet', [CabaneViewController::class, 'showCabaneNidDouillet'])->name('cabane1')->middleware(\App\Http\Middleware\Localisation::class);
 Route::get('/cabaneosmose', [CabaneViewController::class, 'showCabaneOsmose'])->name('cabane2')->middleware(\App\Http\Middleware\Localisation::class);
 Route::get('/cabaneescapade', [CabaneViewController::class, 'showCabaneEscapade'])->name('cabane3')->middleware(\App\Http\Middleware\Localisation::class);
@@ -70,16 +65,11 @@ Route::get('/cabaneeden', [CabaneViewController::class, 'showCabaneEden'])->name
 
 Route::get('/prestations', [PrestationViewController::class, 'showPrestations'])->name('prestations')->middleware(\App\Http\Middleware\Localisation::class);
 
-// Route::post('/reservation/cabanes/disponibilite', [AvailableRoomController::class, 'store'])->name('disponibilite');
 Route::match(['get', 'post'], '/reservation/cabanes/disponibilite', [AvailableRoomController::class, 'store'])->name('disponibilite');
 
 Route::match(['get', 'post'], '/reservation/extras', [ExtrasController::class, 'index'])->name('extras');
 
-// Route::post('/reservation/extras', [ExtrasController::class, 'index'])->name('extras');
-
 Route::match(['get', 'post'], '/reservation/informations/client', [ClientInfoController::class, 'store'])->name('info-client');
-
-// Route::match(['get', 'post'], '/reservation/validate/client', [ClientInfoController::class, 'create'])->name('validate-client');
 
 Route::post('/reservation/validate/client', [ValidateClientController::class, 'store'])->name('validate-client');
 
@@ -91,50 +81,47 @@ Route::get('/reservation/confirmation', function () {
     return view('pages.resa-confirmed');
 })->name('confirmed');
 
-
 Route::get('/moncompte/reservations', [UserReservationController::class, 'index'])->name('user-reservation');
-
 
 Route::get('lang/{lang}', [LanguageController::class, 'switchLang'])->name('lang.switch');
 
+Route::prefix('admin')->middleware(['auth', 'verified'])->group(function () {
 
-Route::get('/admin/reservations', [AdminReservationController::class, 'index'])->name('admin-reservation');
-Route::get('/admin/reservations/details/{id}', [AdminReservationController::class, 'show'])->name('admin-reservation-details');
+    Route::get('/reservations', [AdminReservationController::class, 'index'])->name('admin-reservation');
+    Route::get('/reservations/details/{id}', [AdminReservationController::class, 'show'])->name('admin-reservation-details');
 
+    Route::get('/cabanes', [CabaneController::class, 'index'])->name('afficherCabane');
+    Route::post('/cabanes/create', [CabaneController::class, 'create'])->name('ajouterCabane');
+    Route::get('/cabanes/delete/{id}', [CabaneController::class, 'destroy'])->name('supprimerCabane');
+    Route::get('/cabanes/edit/{id}', [CabaneController::class, 'edit'])->name('editerCabane');
+    Route::post('/cabanes/update/{id}', [CabaneController::class, 'update'])->name('modifierCabane');
 
-Route::get('/admin/cabanes', [CabaneController::class, 'index'])->name('afficherCabane');
-Route::post('/cabanes/create', [CabaneController::class, 'create'])->name('ajouterCabane');
-Route::get('/cabanes/delete/{id}', [CabaneController::class, 'destroy'])->name('supprimerCabane');
-Route::get('/cabanes/edit/{id}', [CabaneController::class,'edit'])->name('editerCabane');
-Route::post('/cabanes/update/{id}', [CabaneController::class,'update'])->name('modifierCabane');
+    Route::get('/equipements', [EquipementController::class, 'index'])->name('afficherEquipement');
+    Route::post('/equipements/create', [EquipementController::class, 'create'])->name('ajouterEquipement');
+    Route::get('/equipements/delete/{id}', [EquipementController::class, 'destroy'])->name('supprimerEquipement');
+    Route::get('/equipements/edit/{id}', [EquipementController::class, 'edit'])->name('editerEquipement');
+    Route::post('/equipements/update/{id}', [EquipementController::class, 'update'])->name('modifierEquipement');
 
-Route::get('/admin/equipements', [EquipementController::class, 'index'])->name('afficherEquipement');
-Route::post('/equipements/create', [EquipementController::class, 'create'])->name('ajouterEquipement');
-Route::get('/equipements/delete/{id}', [EquipementController::class, 'destroy'])->name('supprimerEquipement');
-Route::get('/equipements/edit/{id}', [EquipementController::class,'edit'])->name('editerEquipement');
-Route::post('/equipements/update/{id}', [EquipementController::class,'update'])->name('modifierEquipement');
+    Route::get('/prestations', [PrestationController::class, 'index'])->name('afficherPrestation');
+    Route::post('/prestations/create', [PrestationController::class, 'create'])->name('ajouterPrestation');
+    Route::post('/category/create', [PrestationController::class, 'createCategory'])->name('ajouterCategorie');
+    Route::get('/prestations/delete/{id}', [PrestationController::class, 'destroy'])->name('supprimerPrestation');
+    Route::get('/prestations/edit/{id}', [PrestationController::class, 'edit'])->name('editerPrestation');
+    Route::post('/prestations/update/{id}', [PrestationController::class, 'update'])->name('modifierPrestation');
 
-Route::get('/admin/prestations', [PrestationController::class, 'index'])->name('afficherPrestation');
-Route::post('/prestations/create', [PrestationController::class, 'create'])->name('ajouterPrestation');
-Route::post('/category/create', [PrestationController::class, 'createCategory'])->name('ajouterCategorie');
-Route::get('/prestations/delete/{id}', [PrestationController::class, 'destroy'])->name('supprimerPrestation');
-Route::get('/prestations/edit/{id}', [PrestationController::class,'edit'])->name('editerPrestation');
-Route::post('/prestations/update/{id}', [PrestationController::class,'update'])->name('modifierPrestation');
+    Route::get('/newsletter', [NewsletterController::class, 'index'])->name('afficherEmails');
+    Route::get('/newsletter/delete/{id}', [NewsletterController::class, 'destroy'])->name('supprimerEmail');
+    Route::post('/newsletter/inscription', [NewsletterController::class, 'create'])->name('ajouterNewsletter');
 
-Route::get('/newsletter', [NewsletterController::class, 'index'])->name('afficherEmails');
-Route::get('/newsletter/delete/{id}', [NewsletterController::class, 'destroy'])->name('supprimerEmail');
-Route::post('/newsletter/inscription', [NewsletterController::class,'create'])->name('ajouterNewsletter');
+    Route::post('/demandecontact', [ContactRequestController::class, 'store'])->name('contact-request');
+    Route::get('/contact&acces', [ContactRequestController::class, 'index'])->name('contact')
+        ->middleware(\App\Http\Middleware\Localisation::class);
 
-Route::post('/demandecontact', [ContactRequestController::class, 'store'])->name('contact-request');
-Route::get('/contact&acces', [ContactRequestController::class, 'index'])->name('contact')->middleware(\App\Http\Middleware\Localisation::class);
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
