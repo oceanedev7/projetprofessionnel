85d90bfbf512d1f897cfa28cc2b2a50a953c6746
oceanedev7 <oceanepy7@gmail.com>
2024-11-19 09:09:50 -0400

    modification du href des demandes de contact espace admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -50,7 +50,7 @@
                 <i class="fa-solid fa-arrow-right text-2xl relative right-8"></i>
             </a>
 
-            <a  href="https://mail.google.com/mail/u/3/?ogbl#inbox" target="_blank" class=" flex items-center justify-between bg-white border border-2 border-custom-marron dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <a  href="https://mail.google.com/mail/u/2/?ogbl#inbox" target="_blank" class=" flex items-center justify-between bg-white border border-2 border-custom-marron dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-custom-marron dark:text-gray-100 flex space-x-4">
                     <i class="fa-solid fa-envelope text-lg"></i>
                    <div class="text-lg font-bold"> Voir les demandes de contact </div>
